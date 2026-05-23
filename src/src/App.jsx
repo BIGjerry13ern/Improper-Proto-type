2d79d14afd1a7788f7f4b0981ab4a92adc25522d
Jeremyah Jaramillo <Jeremyah.jaramillo.coding@gmail.com>
2026-05-23 17:01:10 -0500

    Create src/App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GameEngine from './components/GameEngine';
+
+export default function App() {
+  return (
+    <div style={{ margin: 0, padding: 0, backgroundColor: '#121212' }}>
+      <GameEngine />
+    </div>
+  );
+}
