6aa4c4d8b0c272e1c3c146df51dc9c396bc1b945
Jeremyah Jaramillo <Jeremyah.jaramillo.coding@gmail.com>
2026-05-23 16:39:29 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,24 @@
-// Improper Project: Firebase Initialization
-// This file is currently empty, but ready for your config keys later.
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-export const db = null;
-export const auth = null;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz8i2uabTupYrtdepO68j7w72fzIXphNI",
+  authDomain: "improper-prototype.firebaseapp.com",
+  projectId: "improper-prototype",
+  storageBucket: "improper-prototype.firebasestorage.app",
+  messagingSenderId: "94761416130",
+  appId: "1:94761416130:web:f61a22004ee846d521525b",
+  measurementId: "G-MHC70N22X6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
